856a004ed8290ee7f93856d6ab9d3ea1ad390761
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2022-04-12 14:02:04 -0400

    everything just worked :O

--- run.bat
+++ run.bat
@@ -1,21 +1,22 @@
-#boot#
+::boot
 nasm "Boot/boot.asm" -f bin -o "Bin/boot.bin" -i Boot
 
 nasm "Kernel/zeroes.asm" -f bin -o "Bin/zeroes.bin"
 
-#kernel#
+::kernel
 g++ -ffreestanding -m32 -g -c "Kernel/kernel.cpp" -o "Bin/kernel.o" -I Kernel
 
 nasm "Kernel/kernelent.asm" -f elf -o "Bin/kernelent.o" -i Kernel
 
-#linker (temporary fix)#
+::linker (temporary fix)
 ld -o "Bin/fullkernel.tmp" -Ttext 0x1000 "Bin/kernelent.o" "Bin/kernel.o"
 objcopy -O binary -j .text "Bin/fullkernel.tmp" "Bin/fullkernel.bin" 
 
-#final#
-cat "Bin/boot.bin", "Bin/fullkernel.bin" , "Bin/zeroes.bin" > OS.bin
+::final
 
 cd Bin
+
+copy /b boot.bin + /b fullkernel.bin + /b zeroes.bin OS.bin
 
 del boot.bin, fullkernel.bin, kernel.o, kernelent.o, zeroes.bin, fullkernel.tmp
 
